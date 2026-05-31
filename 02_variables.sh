c65209f81063589418ce4d374c9b00bf734856f8
Virender Singh Rawat <virender.7@live.com>
2026-05-31 08:51:25 +0000

    variables script added

---
+++ 02_variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# varaible, expansion and command substitution $(..)
+
+name="hyper"
+tool="wood"
+
+echo "name : $name."
+echo "tool : $tool."
+echo "current date script ran : $(date)"
+echo "shell used $SHELL"
+
